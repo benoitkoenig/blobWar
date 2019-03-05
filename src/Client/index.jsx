a33af03c6382d02b37aa8ff1d103410c9ef38057
Benoit Koenig <koenigbenoit47@gmail.com>
2019-03-05 18:30:10 +0900

    Re-organize import order in root react file

--- src/Client/index.jsx
+++ src/Client/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-/* I'm thinking of taking off react-router, as it creates more problems than it solves. I'm actually only keeping it because this whole project aims at testing new tools */
-import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from "react-redux";
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import store from "./Reducers/store.js";
 
 import Header from "./Components/Header/index.jsx";
 import Alert from "./Components/Alert/index.jsx";
 import Menu from "./Components/Menu/index.jsx";
 import CardsSwitch from "./Components/CardsSwitch/index.jsx";
 import Board from "./Components/Board/index.jsx";
+
+import store from "./Reducers/store.js";
 
 import "./StyleSheets/index.scss";
 
